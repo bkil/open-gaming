c11fe4433c2d6897facbfcd3fe9003865e718bc7
bkil <b.K.il.h.u+tigbuh@gmail.com>
2024-01-16 00:10:52 +0100

    bin/record-curseofwar.sh: save RANDOM seed for recording in command line

--- bin/record-curseofwar.sh
+++ bin/record-curseofwar.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-"$(dirname "$(readlink -f "$0")")"/asciinema.sh \
-  curseofwar -q 1 -i 4 -s n -d hh -W 19 -H 15
+SEED="$((2*(RANDOM * 32768 + RANDOM) + RANDOM % 2))"
+exec "$(dirname "$(readlink -f "$0")")"/asciinema.sh \
+  curseofwar -q 4 -i 4 -s n -d hh -W 19 -H 15 -R "$SEED"
 
 # -T # >= 80x28
